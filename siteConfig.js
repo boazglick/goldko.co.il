d3ff04d006ea77486097188c0ada43918247cc8e
boazglick <95533522+boazglick@users.noreply.github.com>
2022-06-06 14:05:23 +0300

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,23 +1,23 @@
 module.exports = {
-  siteUrl: "https://buzz-tv.co.il", // Site domain. Do not include a trailing slash!
+  siteUrl: "https://goldko.co.il", // Site domain. Do not include a trailing slash!
 
   postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-  siteTitleMeta: "buzz-tv", // This allows an alternative site title for meta data for pages.
+  siteTitleMeta: "goldko", // This allows an alternative site title for meta data for pages.
   siteDescriptionMeta:
-    "buzz-tv11", // This allows an alternative site description for meta data for pages.
+    "goldko", // This allows an alternative site description for meta data for pages.
 
   shareImageWidth: 1000, // Change to the width of your default share image
   shareImageHeight: 523, // Change to the height of your default share image
 
-  shortTitle: "buzz-tv11", // Used for App manifest e.g. Mobile Home Screen
+  shortTitle: "goldko", // Used for App manifest e.g. Mobile Home Screen
   siteIcon: "favicon.png", // Logo in /static dir used for SEO, RSS, and App manifest
   backgroundColor: "#e9e9e9", // Used for Offline Manifest
   themeColor: "#15171A", // Used for Offline Manifest
   apiUrl: "https://wordpress.theasdfghjkl.com",
   subscribeWidget: {
     visible: true,
-    title: "buzz-tv",
+    title: "goldko",
     helpText: "הישארו מעודכנים",
     successMessage: "תודה ההודעה נשלחה בהצלחה",
   },
@@ -25,11 +25,11 @@ module.exports = {
     navigation: [
       {
         label: "דף הבית",
-        url: "https://buzz-tv.co.il/",
+        url: "https://goldko.co.il/",
       },
       {
         label: "יצירת קשר",
-        url: "https://buzz-tv.co.il/contact",
+        url: "https://goldko.co.il/contact",
       },
     ],
   },
@@ -38,16 +38,16 @@ module.exports = {
     navigation: [
       {
         label: "דף הבית",
-        url: "https://buzz-tv.co.il/",
+        url: "https://goldko.co.il/",
       },
       {
         label: "Sitemap",
-        url: "https://buzz-tv.co.il/sitemap.xml",
+        url: "https://goldko.co.il/sitemap.xml",
       },
       
       {
         label: "יצירת קשר",
-        url: "https://buzz-tv.co.il/contact",
+        url: "https://goldko.co.il/contact",
       },
      
     ],
@@ -66,13 +66,13 @@ module.exports = {
     whatsapp: "",
   },
   contactWidget: {
-    title: "Contact Built with Draftbox",
+    title: "https://goldko.co.il/",
     successMessage: "We’ll get in touch with you soon.",
   },
   metadata: {
-    title: "buzz-tv",
+    title: "goldko",
     description:
-      "buzz-tv55",
+      "goldko",
   },
   twitterCard: {
     title: "",
@@ -88,7 +88,7 @@ module.exports = {
     imageUrl: "facebookImage.png",
     appId: "2391725224459953",
   },
-  siteTitle: "buzz-tv",
+  siteTitle: "goldko",
   siteDescription:
     "buzz-tv66",
   language: "he",
